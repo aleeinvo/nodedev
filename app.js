148189f4fa99d25827b10e382b429f29964eca84
Alee Dhillon <ali.asghar@invozone.com>
2022-04-29 21:12:33 +0500

    path

--- app.js
+++ app.js
@@ -5,7 +5,4 @@ const os = require('os');
 let file = path.join(__dirname, 'data.json');
 let log = path.join(__dirname, 'log.txt');
 
-fs.rmSync('test_dir', {
-    recursive: true,
-    force: true
-});
+console.log(path.resolve('.'));
