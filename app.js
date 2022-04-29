a330b6ac08e0f8b4639895121228b4390e73e263
Alee Dhillon <ali.asghar@invozone.com>
2022-04-29 19:56:30 +0500

    get all the files from any dir

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const os = require('os');
 let file = path.join(__dirname, 'data.json');
 let log = path.join(__dirname, 'log.txt');
 
-fs.appendFile(log, '[' + (new Date()).toISOString() + '] ' + JSON.stringify({
-    name: 'Ali Dhillon',
-    username: 'aleedhillon',
-    city: 'Lahore'
-}) + os.EOL, error => {
-    if(error) {
-        console.log(error.message);
-        return;
-    }
-
-    console.log('file has been written successfully');
+const files = fs.readdirSync(__dirname).map(file => {
+    return path.join(__dirname, file);
+}).filter(file => {
+    return fs.lstatSync(file).isFile();
 });
+
+// console.log(files);
+
+console.log(fs.lstatSync('./log.json'));
